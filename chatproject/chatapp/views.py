df7232294353cf399cd3e1931c979942ef8016d0
GOKUL <ramgokulram61@gmail.com>
2021-10-26 20:24:03 +0530

    Seventh Commit: Adding profile page

--- chatproject/chatapp/views.py
+++ chatproject/chatapp/views.py
@@ -1,7 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+from .forms import CustomUserCreationForm
 
+@login_required
 def index(request):
     return render(request, 'index.html')
 
+@login_required
 def room(request, room_name):
     return render(request, 'room.html', {'room_name': room_name})
+
+def register(request):
+    if request.method == "GET":
+        return render(request, "users/register.html", {"form": CustomUserCreationForm})
+    elif request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+
+        if form.is_valid():
+
+            fs = form.save(commit=False)
+            emailvalue = form.cleaned_data.get("email")
+            uservalue = form.cleaned_data.get("username")
+            passwordvalue1 = form.cleaned_data.get("password1")
+            passwordvalue2 = form.cleaned_data.get("password2")
+
+            try:
+                user = User.objects.get(username=uservalue)
+                context = {'form': form,
+                           'error': 'The username you entered has already been taken. Please try another username.'}
+                return render(request, "users/register.html", context)
+
+            except User.DoesNotExist:
+                user = User.objects.create_user(uservalue, password=passwordvalue1, email=emailvalue)
+
+            user.save()
+            login(request, user)
+            return redirect('index')
+        return redirect('register')
+@login_required
+def profile(request, user_name):
+    user = User.objects.get(username=user_name)
+    return render(request, 'profile.html', {'user_name': user})
--- chatproject/chatproject/urls.py
+++ chatproject/chatproject/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+import chatapp
 
 urlpatterns = [
     path('chat/', include('chatapp.urls')),
     path('accounts/', include("django.contrib.auth.urls")),
     path('admin/', admin.site.urls),
+    path('register/', chatapp.views.register, name='register'),
 ]
